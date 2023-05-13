d4e23291f8195ebaa9391a95372bc2a557115c2d
Lukman <lukmanmirmanov@gmail.com>
2023-05-14 02:06:16 +0500

    first commit

---
+++ src/main/java/com/example/FootbalManager/FootbalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootbalManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootbalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootbalManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FootbalManager/FootbalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FootbalManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
